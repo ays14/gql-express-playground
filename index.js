a8f6f9347b01859e2827adecb9cd9a3312d84f2c
ays14 <ayush.sh143@gmail.com>
2018-08-12 18:37:09 +0530

    Add index.js - module to integrate GraphQL playground

---
+++ index.js
@@ -0,0 +1,33 @@
+"use strict";
+
+var playgroundHtml = require("graphql-playground-html");
+var playgroundVersion = require('../package.json').playgroundVersion;
+
+var __assign = (this && this.__assign) || Object.assign || function(a) {
+    for (var b, i = 1, n = arguments.length; i < n; i++) {
+        b = arguments[i];
+        for (var m in b) if (Object.prototype.hasOwnProperty.call(b, m))
+            a[m] = s[m];
+    }
+    return a;
+};
+
+Object.defineProperty(
+	exports, "__esModule",
+	{ value: true }
+);
+
+var playground = {
+    expressPlayground : function voyagerExpress(options) {
+                            var middlewareOptions = __assign({}, options, { version: playgroundVersion });
+                            return function (req, res, next) {
+                                res.setHeader('Content-Type', 'text/html');
+                                var playground = playgroundHtml.renderPlaygroundPage(middlewareOptions);
+                                res.write(playground);
+                                res.end();
+                                next();
+                            };
+                        }
+}
+
+module.exports = playground;
